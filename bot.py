4ebd0a7fb4220cd3e9eb82d253062c071af97c01
weinbergernikita <weinbergernikita@gmail.com>
2026-02-19 22:08:03 +0700

    Worktest9

--- bot.py
+++ bot.py
@@ -49,7 +49,7 @@ def validate_phone(phone):
 def get_main_menu():
     """Главное меню"""
     buttons = [
-        [InlineKeyboardButton("🆘 Создать заявку", callback_data="create")],
+        [InlineKeyboardButton("🆘 Создать заявкухехе", callback_data="create")],
         [InlineKeyboardButton("📞 Контакты", callback_data="contacts")],
         [InlineKeyboardButton("💰 Цены", callback_data="prices")]
     ]
